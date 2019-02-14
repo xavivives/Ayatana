d675869f4782cab43f0a70846a073dc0b2d851e6
Xavi <xavings@gmail.com>
2019-02-14 09:01:52 +0000

    Basic ws setup. NodeData interfaces

--- src/sensorRegistry.ts
+++ src/sensorRegistry.ts
@@ -1,39 +1,73 @@
+import * as WebSocket from "ws"
+
 export enum connectionType {
     ws = "ws",
     http = "http"
 }
 
-export interface ISensorData {
+export interface INodeMetadata {
     id: string
-    ip: string
+    url: string
     connection: connectionType
     frequency: number
 }
 
+export interface INodeData {
+    err: string
+    sensors: ISensor[]
+}
+
+export interface INodeStatus{
+    error : string
+    metadata : INodeMetadata
+    currentTimestamp: number
+}
+
+export interface ISensor {
+    id: string
+    values: ITimestampedRecord[]
+}
+
+export interface ITimestampedRecord {
+    timestamp: number
+    value: number
+}
+
 export class SensorRegistry {
 
-    private registry: ISensorData[]
+    private registry: INodeMetadata[]
 
-    public popuplate = (data: ISensorData[]) => {
+    public popuplate = (data: INodeMetadata[]) => {
         this.registry = data
     }
 
     public dial = () => {
-        for (const sensor of this.registry) {
-            if (sensor.connection === connectionType.http) {
-                this.connectHttpSensor(sensor)
+        for (const node of this.registry) {
+            if (node.connection === connectionType.http) {
+                this.connectHttpNode(node)
             }
-            else if (sensor.connection === connectionType.ws) {
-                this.connectWsSensor(sensor)
+            else if (node.connection === connectionType.ws) {
+                this.connectWsNode(node)
             }
         }
     }
 
-    public connectHttpSensor = (sensor: ISensorData) => {
+    public connectHttpNode = (node: INodeMetadata) => {
 
     }
 
-    public connectWsSensor = (sensor: ISensorData) => {
+    public connectWsNode = (node: INodeMetadata) => {
+
+        const ws = new WebSocket(node.url);
+
+        ws.on('open', () => {
+            ws.send('something');
+            console.log(`Connection to ${node.id} stablished`)
+        });
+
+        ws.on('message', (data:INodeData) => {
+            console.log(data);
+        });
     }
 }
 
