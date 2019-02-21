ccb8846f034bf64321e3a6b0dc7d2ffd757c1168
Xavi <xavings@gmail.com>
2019-02-21 21:10:29 +0000

    compiling script upgrade

--- node/src/upload.sh
+++ node/src/upload.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-USB_PORT ="/dev/ttyACM0"
+USB_PORT="/dev/ttyACM0"
 FQBN="esp8266:esp8266:nodemcu"
+DIR="ota"
+MODE=$1
 
-echo "Uploading..."
+cd $DIR
 
-arduino-cli upload -p $USB_PORT --fqbn $FQBN
+if [ "$MODE" = "compile" ] || [ -z "$MODE" ]
+then
+    echo "Compiling..."
+    arduino-cli compile --fqbn $FQBN
+fi
+
+if [ "$MODE" = "upload" ] || [ -z "$MODE" ]
+then
+    echo "Uploading..."
+    arduino-cli upload -p $USB_PORT --fqbn $FQBN
+fi
+cd ../
+
+exit 1
